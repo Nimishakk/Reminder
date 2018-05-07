bbb11d5d135183d24ee9907615f913c19550ed1d
Nimishakk <37541336+Nimishakk@users.noreply.github.com>
2018-05-07 17:12:11 +0530

    Reminder application using windows forms

---
+++ WindowsFormsApplication1/Reminder.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Reminder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dt_startdate = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dt_enddate = new System.Windows.Forms.DateTimePicker();
+            this.txt_event = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(65, 258);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 0;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Event";
+            // 
+            // dt_startdate
+            // 
+            this.dt_startdate.Location = new System.Drawing.Point(171, 119);
+            this.dt_startdate.Name = "dt_startdate";
+            this.dt_startdate.Size = new System.Drawing.Size(238, 20);
+            this.dt_startdate.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Start Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "End Date";
+            // 
+            // dt_enddate
+            // 
+            this.dt_enddate.Location = new System.Drawing.Point(171, 162);
+            this.dt_enddate.Name = "dt_enddate";
+            this.dt_enddate.Size = new System.Drawing.Size(238, 20);
+            this.dt_enddate.TabIndex = 5;
+            // 
+            // txt_event
+            // 
+            this.txt_event.Location = new System.Drawing.Point(171, 74);
+            this.txt_event.Name = "txt_event";
+            this.txt_event.Size = new System.Drawing.Size(238, 20);
+            this.txt_event.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(426, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(536, 359);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Edit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Reminder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1059, 428);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_event);
+            this.Controls.Add(this.dt_enddate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dt_startdate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnsave);
+            this.Name = "Reminder";
+            this.Text = "Reminder";
+            this.Load += new System.EventHandler(this.Reminder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dt_startdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dt_enddate;
+        private System.Windows.Forms.TextBox txt_event;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Reminder.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Reminder : Form
+    {
+        SqlConnection db_Connection = new SqlConnection();
+        SqlCommand db_Command = new SqlCommand();
+        SqlDataAdapter db_Adapter = new SqlDataAdapter();
+        DateTime Wkdate = System.DateTime.Now.AddDays(7);
+        public Reminder()
+        {
+            InitializeComponent();
+            db_Connection.ConnectionString = "Data Source=QIS-ADMIN;Initial Catalog=Information;Integrated Security=True";
+        }
+
+        private void Reminder_Load(object sender, EventArgs e)
+        {
+            dt_startdate.CustomFormat = "mm-dd-yyyy";
+            dt_startdate.CustomFormat = "mm-dd-yyyy";
+            dt_startdate.Format = DateTimePickerFormat.Time;
+            dt_enddate.Format = DateTimePickerFormat.Time;
+            //DataTable dt = GetDataTable("SELECT * FROM Save_Info --where CONVERT(date, End_date) >'" + System.DateTime.Now.Date + "'  and CONVERT(date, Start_date) < '" + Wkdate + "' ");
+            DataTable dt = GetDataTable("SELECT * FROM Save_Info where CONVERT(date, End_date) >'" + System.DateTime.Now.Date + "'  and CONVERT(date, Start_date) < '" + Wkdate + "' ");
+            dataGridView1.DataSource = dt;
+            dataGridView1.Show();
+            //dataGridView1.Columns["Information_id"].Visible = false;
+            //button2.Visible = false;
+
+
+            try
+            {
+                for (int x = 0; x < dataGridView1.Rows.Count - 1; x++)
+                {
+                    string reminder = dataGridView1.Rows[x].Cells[1].Value.ToString();
+                    DateTime startdate = Convert.ToDateTime(dataGridView1.Rows[x].Cells[2].Value.ToString());
+                    //string reminder = dataGridView1.Rows[x].Cells[2].ToString();
+
+                    if (startdate.Date == System.DateTime.Now.Date)
+                    {
+                        MessageBox.Show("you have a notification for the event " + reminder);
+
+
+                    }
+                }
+            }
+            catch (Exception exe)
+            { }
+
+
+
+        }
+
+        public Int32 ExecuteInsert(String DatabaseQuery)
+        {
+            Int32 AffectedRows;
+            try
+            {
+
+                OpenConnection();
+                db_Command = new System.Data.SqlClient.SqlCommand();
+                db_Command.Connection = getconnection();
+                db_Command.CommandType = System.Data.CommandType.Text;
+                db_Command.CommandText = DatabaseQuery;
+                AffectedRows = db_Command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //GlobalAccess.ObjGenerealEL.file_append("DataAccessLayer", ex.Message.ToString(), "ExecuteInsert", "DBFunctionDAL");
+                return -1;
+            }
+            finally
+            {
+
+                CloseConnection();
+            }
+            return AffectedRows;
+        }
+
+
+
+
+        public System.Data.SqlClient.SqlConnection getconnection()
+        {
+
+            return db_Connection;
+        }
+        public void OpenConnection()
+        {
+            try
+            {
+                if (db_Connection.State != System.Data.ConnectionState.Open)
+                {
+                    db_Connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (db_Connection.State != System.Data.ConnectionState.Closed)
+            {
+                db_Connection.Close();
+            }
+        }
+
+
+        public System.Data.DataTable GetDataTable(String SelectQuery)
+        {
+            System.Data.DataTable dt_Table = new System.Data.DataTable();
+
+            try
+            {
+                OpenConnection();
+                db_Adapter = new System.Data.SqlClient.SqlDataAdapter(SelectQuery, db_Connection);
+                //dt_Table.Columns.Add("Select");
+
+                db_Adapter.Fill(dt_Table);
+
+                //DDL.SelectedIndex = 0;
+                //DDL.Items.Insert(0, "-Select-");
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                db_Adapter.Dispose();
+
+                CloseConnection();
+            }
+            return dt_Table;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s = ExecuteInsert("insert into Save_Info values('" + txt_event.Text + "','" + dt_startdate.Value + "','" + dt_enddate.Value + "')");
+            DataTable dt = GetDataTable("SELECT * FROM Save_Info --where CONVERT(date, End_date) >'" + System.DateTime.Now.Date + "'  and CONVERT(date, Start_date) < '" + Wkdate + "' ");
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["Information_id"].Visible = false;
+            dataGridView1.Show();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button2.Visible = true;
+            btnsave.Visible = false;
+            DataGridViewCheckBoxColumn chkPresent = new DataGridViewCheckBoxColumn();
+            dataGridView1.Columns.Add(chkPresent);
+            chkPresent.HeaderText = "Delete";
+            try
+            {
+                for (int x = 0; x < dataGridView1.Rows.Count - 1; x++)
+                {
+                    DataGridViewCheckBoxCell chk = dataGridView1.Rows[x].Cells[0] as DataGridViewCheckBoxCell;
+
+                    if (Convert.ToBoolean(chk.Value) == true)
+                    {
+                        string BatchN = dataGridView1.Rows[x].Cells[1].Value.ToString();
+                        //int res = objclsMasterUpdate.TypeUP(BatchN, Convert.ToInt32(cmbmedtype1.SelectedValue.ToString()));
+                    }
+                }
+                DataTable dt = GetDataTable("SELECT * FROM Save_Info --where CONVERT(date, End_date) >'" + System.DateTime.Now.Date + "'  and CONVERT(date, Start_date) < '" + Wkdate + "' ");
+                dataGridView1.DataSource = dt;
+                dataGridView1.Columns["Information_id"].Visible = false;
+                dataGridView1.Show();
+                button1.Visible = false;
+
+
+
+
+
+            }
+            catch (Exception exe) { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                for (int x = 0; x < dataGridView1.Rows.Count - 1; x++)
+                {
+                    DataGridViewCheckBoxCell chk = dataGridView1.Rows[x].Cells[4] as DataGridViewCheckBoxCell;
+
+                    if (Convert.ToBoolean(chk.Value) == true)
+                    {
+                        string id = dataGridView1.Rows[x].Cells[0].Value.ToString();
+
+                        int res = ExecuteUpdate("Delete from Save_Info where Information_id=" + id + " "); ;
+                    }
+                }
+                DataTable dt = GetDataTable("SELECT * FROM Save_Info --where CONVERT(date, End_date) >'" + System.DateTime.Now.Date + "'  and CONVERT(date, Start_date) < '" + Wkdate + "' ");
+                dataGridView1.DataSource = dt;
+                dataGridView1.Columns["Information_id"].Visible = false;
+                dataGridView1.Show();
+                MessageBox.Show("deleted sucessfully");
+                button2.Visible = false;
+                btnsave.Visible = true;
+                button1.Visible = true;
+
+
+            }
+            catch (Exception exe) { }
+        }
+
+        public Int32 ExecuteUpdate(String DatabaseQuery)
+        {
+            Int32 AffectedRows;
+            try
+            {
+                OpenConnection();
+                db_Command = new System.Data.SqlClient.SqlCommand();
+                db_Command.Connection = getconnection();
+                db_Command.CommandType = System.Data.CommandType.Text;
+                db_Command.CommandText = DatabaseQuery;
+                AffectedRows = db_Command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                // GlobalAccess.ObjGenerealEL.file_append("DataAccessLayer", ex.Message.ToString(), "ExecuteUpdate", "DBFunctionDAL");
+                return -1;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return AffectedRows;
+        }
+
+
+
+
+    }
+}
